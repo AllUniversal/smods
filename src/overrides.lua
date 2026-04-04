13a051f57716392829f9844bbcff8ed11bc1034f
AllUniversal <alluniversal@gmx.ch>
2026-04-04 12:37:28 +0200

    More WIP moving.
    *Title

--- src/overrides.lua
+++ src/overrides.lua
@@ -2719,3 +2719,160 @@ G.FUNCS.change_viewed_back = function(...)
 	end
 	return g_funcs_change_viewed_back_ref(...)
 end
+
+
+-- SMODS.GameState related overrides
+local ease_bg_col_bl_ref = ease_background_colour_blind
+function ease_background_colour_blind(state, blind_override)
+	if SMODS.GameStates[state] and SMODS.GameStates[state].ease_background_colour then
+		return SMODS.GameStates[state]:ease_background_colour(blind_override)
+	end
+	return ease_bg_col_bl_ref(state, blind_override)
+end
+
+function end_round()
+	G.E_MANAGER:add_event(Event({
+		trigger = 'after',
+		delay = 0.2,
+		func = function()
+			G.GAME.blind.in_blind = false
+			local game_over = true
+			local game_won = false
+			G.RESET_BLIND_STATES = true
+			G.RESET_JIGGLES = true
+			if G.GAME.chips - G.GAME.blind.chips >= 0 then
+				game_over = false
+			end
+			-- context.end_of_round calculations
+			SMODS.saved = false
+			G.GAME.saved_text = nil
+			SMODS.calculate_context({end_of_round = true, game_over = game_over, beat_boss = G.GAME.blind.boss })
+			if SMODS.saved then game_over = false end
+			-- TARGET: main end_of_round evaluation
+			if G.GAME.round_resets.ante == G.GAME.win_ante and G.GAME.blind:is_type("Boss") then
+				game_won = true
+				G.GAME.won = true
+			end
+			if game_over then
+				G.STATE = G.STATES.GAME_OVER
+				if not G.GAME.won and not G.GAME.seeded and not G.GAME.challenge then 
+					G.PROFILES[G.SETTINGS.profile].high_scores.current_streak.amt = 0
+				end
+				G:save_settings()
+				G.FILE_HANDLER.force = true
+				G.STATE_COMPLETE = false
+			else
+				G.GAME.unused_discards = (G.GAME.unused_discards or 0) + G.GAME.current_round.discards_left
+				if G.GAME.blind and G.GAME.blind.config.blind then 
+					discover_card(G.GAME.blind.config.blind)
+				end
+
+				if G.GAME.blind:is_type("Boss") then
+					local _handname, _played, _order = 'High Card', -1, 100
+					for k, v in pairs(G.GAME.hands) do
+						if v.played > _played or (v.played == _played and _order > v.order) then 
+							_played = v.played
+							_handname = k
+						end
+					end
+					G.GAME.current_round.most_played_poker_hand = _handname
+				end
+
+				if G.GAME.blind:is_type("Boss") and not G.GAME.seeded and not G.GAME.challenge  then
+					G.GAME.current_boss_streak = G.GAME.current_boss_streak + 1
+					check_and_set_high_score('boss_streak', G.GAME.current_boss_streak)
+				end
+				
+				if G.GAME.current_round.hands_played == 1 then 
+					inc_career_stat('c_single_hand_round_streak', 1)
+				else
+					if not G.GAME.seeded and not G.GAME.challenge  then
+						G.PROFILES[G.SETTINGS.profile].career_stats.c_single_hand_round_streak = 0
+						G:save_settings()
+					end
+				end
+
+				check_for_unlock({type = 'round_win'})
+				set_joker_usage()
+				if game_won and not G.GAME.win_notified then
+					G.GAME.win_notified = true
+					G.E_MANAGER:add_event(Event({
+						trigger = 'immediate',
+						blocking = false,
+						blockable = false,
+						func = (function()
+							if SMODS.GameStates[G.STATE] and SMODS.GameStates[G.STATE].check_win then
+								win_game()
+								G.GAME.won = true
+								return true
+							end
+						end)
+					}))
+				end
+				for _,v in ipairs(SMODS.get_card_areas('playing_cards', 'end_of_round')) do
+					SMODS.calculate_end_of_round_effects({ cardarea = v, end_of_round = true, beat_boss = G.GAME.blind.boss })
+				end
+
+				G.FUNCS.draw_from_hand_to_discard()
+				if G.GAME.blind:is_type("Boss") then
+					G.GAME.voucher_restock = nil
+					for k, v in pairs(G.GAME.hands) do
+						v.played_this_ante = 0
+					end
+					if G.GAME.modifiers.set_eternal_ante and (G.GAME.round_resets.ante == G.GAME.modifiers.set_eternal_ante) then 
+						for k, v in ipairs(G.jokers.cards) do
+							v:set_eternal(true)
+						end
+					end
+					if G.GAME.modifiers.set_joker_slots_ante and (G.GAME.round_resets.ante == G.GAME.modifiers.set_joker_slots_ante) then 
+						G.jokers.config.card_limit = 0
+					end
+					delay(0.4)
+				end
+				G.FUNCS.draw_from_discard_to_deck()
+				G.E_MANAGER:add_event(Event({
+					trigger = 'after',
+					delay = 0.3,
+					func = function()
+						if SMODS.get_current_state().args.trigger_callbacks then
+							SMODS.get_active_ap_node():trigger_callbacks("defeated")
+						end
+						G.STATE_COMPLETE = false
+
+						if G.GAME.round_resets.blind == G.P_BLINDS.bl_small then
+							-- TODO : Check/Replace blind_states
+							G.GAME.round_resets.blind_states.Small = 'Defeated'
+						elseif G.GAME.round_resets.blind == G.P_BLINDS.bl_big then
+							G.GAME.round_resets.blind_states.Big = 'Defeated'
+						else
+							G.GAME.current_round.voucher = SMODS.get_next_vouchers()
+							G.GAME.round_resets.blind_states.Boss = 'Defeated'
+							for k, v in ipairs(G.playing_cards) do
+								v.ability.played_this_ante = nil
+							end
+						end
+
+						if G.GAME.round_resets.temp_handsize then G.hand:change_size(-G.GAME.round_resets.temp_handsize); G.GAME.round_resets.temp_handsize = nil end
+						if G.GAME.round_resets.temp_reroll_cost then G.GAME.round_resets.temp_reroll_cost = nil; calculate_reroll_cost(true) end
+
+						reset_idol_card()
+						reset_mail_rank()
+						reset_ancient_card()
+						reset_castle_card()
+						for _, mod in ipairs(SMODS.mod_list) do
+							if mod.reset_game_globals and type(mod.reset_game_globals) == 'function' then
+								mod.reset_game_globals(false)
+							end
+						end
+						for k, v in ipairs(G.playing_cards) do
+							v.ability.discarded = nil
+							v.ability.forced_selection = nil
+						end
+					return true
+					end
+				}))
+			end
+			return true
+		end
+	}))
+end
